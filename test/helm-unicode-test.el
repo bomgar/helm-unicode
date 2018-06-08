fbeb0c5e741a6f462520884b744d43a9acbe1d34
Patrick Haun <bomgar85@googlemail.com>
2018-06-08 16:11:35 +0200

    [fix] test

--- test/helm-unicode-test.el
+++ test/helm-unicode-test.el
@@ -1,6 +1,6 @@
 
 (ert-deftest helm-unicode-build-candidates-test ()
         (should (equal '("BELL 🔔" "DOWN ARROW ↓") (with-mock
-                     (mock (ucs-names) => '(("DOWN ARROW" . 8595) ("BELL" . 128276))  :times 1)
+                     (mock (ucs-names) => '(("DOWN ARROW" . 8595) ("BELL" . 128276))  :times 2)
                      (helm-unicode-build-candidates)))))
 
