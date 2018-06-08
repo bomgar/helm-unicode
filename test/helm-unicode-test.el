a0bf71b95e7bc7101082172684a0ac3efa260ca8
Patrick Haun <bomgar85@googlemail.com>
2018-06-08 16:19:29 +0200

    [test] new emacs26 problem

--- test/helm-unicode-test.el
+++ test/helm-unicode-test.el
@@ -1,6 +1,16 @@
 
 (ert-deftest helm-unicode-build-candidates-test ()
-        (should (equal '("BELL 🔔" "DOWN ARROW ↓") (with-mock
-                     (mock (ucs-names) => '(("DOWN ARROW" . 8595) ("BELL" . 128276))  :times 2)
-                     (helm-unicode-build-candidates)))))
+  (should (equal '("BELL 🔔" "DOWN ARROW ↓") (with-mock
+                                              (mock (ucs-names) => '(("DOWN ARROW" . 8595) ("BELL" . 128276)))
+                                              (helm-unicode-build-candidates)))))
+
+(ert-deftest helm-unicode-build-candidates-test-emacs26 ()
+  (let ((myHash (make-hash-table :test 'equal)))
+    (puthash "DOWN ARROW" 8595 myHash)
+    (puthash "BELL" 128276 myHash)
+    (should (equal '("BELL 🔔" "DOWN ARROW ↓") (with-mock
+                                                (mock (ucs-names) => myHash)
+                                                (with-mock
+                                                 (mock (hash-table-keys myHash) => '("DOWN ARROW" "BELL"))
+                                                 (helm-unicode-build-candidates)))))))
 
